ee295975d534502c11d296e771acb0746a397b35
Germano Gabbianelli <tyrion.mx@gmail.com>
2013-11-28 12:57:48 +0100

    Removed plug app, some cms plugins and all comments from settings.py.

--- bug/settings.py
+++ bug/settings.py
@@ -1,34 +1,15 @@
-"""
-Django settings for bug project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-
-"""
 from django.conf import global_settings
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -38,26 +19,19 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #'djangocms_text_ckeditor',
     'cms',
     'cms.stacks',
     'mptt',
     'menus',
     'south',
     'sekizai',
-    'djangocms_admin_style',
 
+    'djangocms_admin_style',
     'django.contrib.admin',
 
-    # PLUGINS:
-    'cms.plugins.googlemap',
     'cms.plugins.link',
     'cms.plugins.picture',
-    'cms.plugins.snippet',
-    'cms.plugins.teaser',
-    'cms.plugins.video',
 
-    'plug',
     'app',
 )
 
@@ -94,8 +68,6 @@ ROOT_URLCONF = 'bug.urls'
 WSGI_APPLICATION = 'bug.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -104,14 +76,12 @@ DATABASES = {
     }
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en'
-LANGUAGES = [
+LANGUAGES = (
     ('en', 'English'),
     ('it', 'Italiano')
-]
+)
 
 TIME_ZONE = 'UTC'
 
@@ -122,8 +92,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'var', 'www')
